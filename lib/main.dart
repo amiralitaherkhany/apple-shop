81f10d5fe472ab7d596af67bcba85c1d51979ab6
Your Name <you@example.com>
2024-08-21 14:20:30 +0330

    created list item for category list

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:apple_shop/constants/colors.dart';
-import 'package:apple_shop/widgets/banner_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +9,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              BannerSlider(),
-            ],
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  width: 56,
+                  height: 56,
+                  decoration: ShapeDecoration(
+                    shadows: const [
+                      BoxShadow(
+                        offset: Offset(0, 2.77),
+                        blurRadius: 2.21,
+                        spreadRadius: 0,
+                        color: Color.fromRGBO(251, 173, 64, 0.04),
+                      ),
+                      BoxShadow(
+                        offset: Offset(0, 6.65),
+                        blurRadius: 5.32,
+                        spreadRadius: 0,
+                        color: Color.fromRGBO(251, 173, 64, 0.04),
+                      ),
+                      BoxShadow(
+                        offset: Offset(0, 12.52),
+                        blurRadius: 10.02,
+                        spreadRadius: 0,
+                        color: Color.fromRGBO(251, 173, 64, 0.05),
+                      ),
+                      BoxShadow(
+                        offset: Offset(0, 22.34),
+                        blurRadius: 17.87,
+                        spreadRadius: 0,
+                        color: Color.fromRGBO(251, 173, 64, 0.05),
+                      ),
+                      BoxShadow(
+                        offset: Offset(0, 41.78),
+                        blurRadius: 33.42,
+                        spreadRadius: 0,
+                        color: Color.fromRGBO(251, 173, 64, 0.05),
+                      ),
+                      BoxShadow(
+                        offset: Offset(0, 100),
+                        blurRadius: 80,
+                        spreadRadius: 0,
+                        color: Color.fromRGBO(251, 173, 64, 0.07),
+                      ),
+                    ],
+                    shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    color: const Color.fromRGBO(251, 173, 64, 1),
+                  ),
+                  child: const Icon(
+                    Icons.ads_click,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'همه',
+                  style: TextStyle(
+                    fontFamily: 'SB',
+                    color: Colors.black,
+                    fontSize: 12,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
