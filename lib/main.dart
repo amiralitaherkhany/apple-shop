825d318219769d9f1f70ba0160572b81e692b728
Your Name <you@example.com>
2024-08-22 19:23:42 +0330

    created bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:apple_shop/constants/colors.dart';
 import 'package:apple_shop/screens/category_screen.dart';
+import 'package:apple_shop/screens/home_screen.dart';
 import 'package:apple_shop/screens/product_list_screen.dart';
+import 'package:ficonsax/ficonsax.dart';
 
 import 'package:flutter/material.dart';
 
@@ -23,7 +27,77 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: const ProductListScreen(),
+      home: Scaffold(
+        extendBody: true,
+        bottomNavigationBar: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 10,
+              sigmaY: 10,
+            ),
+            child: Container(
+              height: 75,
+              decoration: const BoxDecoration(
+                backgroundBlendMode: BlendMode.overlay,
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromRGBO(255, 255, 255, 0.4),
+                    Color.fromRGBO(255, 255, 255, 0.1),
+                  ],
+                ),
+              ),
+              child: BottomNavigationBar(
+                currentIndex: 3,
+                selectedItemColor: MyColors.myBlue,
+                unselectedItemColor: Colors.black,
+                selectedFontSize: 20,
+                unselectedFontSize: 10,
+                showSelectedLabels: true,
+                unselectedLabelStyle: const TextStyle(
+                  fontFamily: 'SB',
+                  fontSize: 10,
+                ),
+                selectedLabelStyle: const TextStyle(
+                  fontFamily: 'SB',
+                  fontSize: 10,
+                ),
+                unselectedIconTheme: const IconThemeData(
+                  color: Colors.black,
+                ),
+                showUnselectedLabels: true,
+                selectedIconTheme: const IconThemeData(
+                  color: MyColors.myBlue,
+                ),
+                iconSize: 30,
+                elevation: 0,
+                type: BottomNavigationBarType.fixed,
+                backgroundColor: Colors.transparent,
+                items: const [
+                  BottomNavigationBarItem(
+                    label: 'حساب کاربری',
+                    icon: Icon(IconsaxOutline.profile_circle),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(IconsaxOutline.bag_2),
+                    label: 'سبد خرید',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(IconsaxOutline.category_2),
+                    label: 'دسته بندی',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(IconsaxBold.home),
+                    label: 'خانه',
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: const HomeScreen(),
+      ),
     );
   }
 }
