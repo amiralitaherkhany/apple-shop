41834b775b750d906c94228cc3b9b625e071ce37
Your Name <you@example.com>
2024-08-29 13:14:42 +0330

    designed banner infinite auto scroll

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -1,19 +1,53 @@
+import 'dart:async';
+
 import 'package:apple_shop/constants/colors.dart';
 import 'package:apple_shop/models/banner.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class BannerSlider extends StatelessWidget {
+class BannerSlider extends StatefulWidget {
   const BannerSlider({
     super.key,
     required this.bannerList,
   });
   final List<BannerModel> bannerList;
+
+  @override
+  State<BannerSlider> createState() => _BannerSliderState();
+}
+
+class _BannerSliderState extends State<BannerSlider> {
+  late Timer timer;
+  static int currentPage = 0;
+  final PageController pageController =
+      PageController(viewportFraction: 0.9, initialPage: currentPage);
+  @override
+  void initState() {
+    timer = Timer.periodic(
+      const Duration(seconds: 5),
+      (Timer timer) {
+        if (currentPage < widget.bannerList.length - 1) {
+          currentPage++;
+        } else {
+          currentPage = 0;
+        }
+        pageController.animateToPage(currentPage,
+            duration: const Duration(seconds: 1), curve: Easing.legacy);
+      },
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    PageController pageController =
-        PageController(viewportFraction: 0.9, initialPage: 1);
     return SizedBox(
       height: 177,
       child: Stack(
@@ -21,7 +55,7 @@ class BannerSlider extends StatelessWidget {
         children: [
           PageView.builder(
             controller: pageController,
-            itemCount: bannerList.length,
+            itemCount: widget.bannerList.length,
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.only(left: 10.0, right: 10),
@@ -34,7 +68,7 @@ class BannerSlider extends StatelessWidget {
                     errorWidget: (context, url, error) => Container(
                       color: Colors.red[500],
                     ),
-                    imageUrl: bannerList[index].thumbnail!,
+                    imageUrl: widget.bannerList[index].thumbnail!,
                     width: 340,
                     height: 177,
                     fit: BoxFit.cover,
@@ -47,7 +81,7 @@ class BannerSlider extends StatelessWidget {
             bottom: 8,
             child: SmoothPageIndicator(
               controller: pageController,
-              count: bannerList.length,
+              count: widget.bannerList.length,
               effect: const ExpandingDotsEffect(
                 expansionFactor: 5,
                 activeDotColor: MyColors.myBlue,
